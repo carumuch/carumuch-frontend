114b80f52000aa2feb7ed943509cec6df5a77807
yeo99 <qw6375@gmail.com>
2024-08-17 15:23:33 +0900

    #2 Add: 커스텀 모달 작성, axios 인스턴스 코드 작성

---
+++ src/app/(beforelogin)/signup/page.tsx
@@ -0,0 +1,4 @@
+'use client';
+
+// import { useState } from 'react';
+// import { useMutation } from '@tanstack/react-query'
---
+++ src/components/modal/CustomModal.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { ReactNode } from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  // useDisclosure,
+} from '@chakra-ui/react';
+
+interface CustomModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  body: ReactNode;
+  footer?: ReactNode;
+}
+
+function CustomModal({
+  isOpen,
+  onClose,
+  title,
+  body,
+  footer,
+}: CustomModalProps) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>{body}</ModalBody>
+        <ModalFooter>
+          {footer || (
+            <Button colorScheme="blud" onClick={onClose}>
+              닫기
+            </Button>
+          )}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+CustomModal.defaultProps = {
+  footer: null,
+};
+
+export default CustomModal;
