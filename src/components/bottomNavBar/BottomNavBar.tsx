d90308c3579abc865913627d3144aed16555e103
yeo99 <qw6375@gmail.com>
2024-10-09 01:17:22 +0900

    #4 Feat: 메뉴 네비게이션 하이라이팅 기능 추가

--- src/components/bottomNavBar/BottomNavBar.tsx
+++ src/components/bottomNavBar/BottomNavBar.tsx
@@ -2,23 +2,26 @@
 
 import { Box, Flex, IconButton, Text } from '@chakra-ui/react';
 import { AtSignIcon } from '@chakra-ui/icons';
+import { useRouter, usePathname } from 'next/navigation';
 
 interface NavItemProps {
   icon: React.ElementType;
   label: string;
+  isSelected: boolean;
+  onClick: () => void;
 }
 
-function NavItem({ icon, label }: NavItemProps) {
+function NavItem({ icon, label, isSelected, onClick }: NavItemProps) {
   return (
-    <Box textAlign="center" position="relative">
+    <Box textAlign="center" position="relative" onClick={onClick}>
       <IconButton
         aria-label={label}
         icon={<Box as={icon} w={6} h={6} />}
         variant="ghost"
-        color="gray.600"
+        color={isSelected ? 'blue.600' : 'gray.600'} // 활성화된 경우 흰색으로
         _hover={{ bg: 'gray.700', color: 'white' }}
       />
-      <Text fontSize="sm" color="gray.600" mt={1}>
+      <Text fontSize="sm" color={isSelected ? 'blue.600' : 'gray.600'} mt={1}>
         {label}
       </Text>
     </Box>
@@ -26,6 +29,13 @@ function NavItem({ icon, label }: NavItemProps) {
 }
 
 function BottomNavBar() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleSelect = (href: string) => {
+    router.push(href);
+  };
+
   return (
     <Flex
       as="nav"
@@ -42,11 +52,36 @@ function BottomNavBar() {
       zIndex="1000"
       mx="auto"
     >
-      <NavItem icon={AtSignIcon} label="메인" />
-      <NavItem icon={AtSignIcon} label="수리입찰" />
-      <NavItem icon={AtSignIcon} label="커뮤니티" />
-      <NavItem icon={AtSignIcon} label="입찰관리" />
-      <NavItem icon={AtSignIcon} label="내 정보" />
+      <NavItem
+        icon={AtSignIcon}
+        label="메인"
+        isSelected={pathname.startsWith('/main')}
+        onClick={() => handleSelect('/main')}
+      />
+      <NavItem
+        icon={AtSignIcon}
+        label="수리입찰"
+        isSelected={pathname.startsWith('/repair')}
+        onClick={() => handleSelect('/repair')}
+      />
+      <NavItem
+        icon={AtSignIcon}
+        label="커뮤니티"
+        isSelected={pathname.startsWith('/community')}
+        onClick={() => handleSelect('/community')}
+      />
+      <NavItem
+        icon={AtSignIcon}
+        label="입찰관리"
+        isSelected={pathname === '/bid'}
+        onClick={() => handleSelect('/bid')}
+      />
+      <NavItem
+        icon={AtSignIcon}
+        label="내 정보"
+        isSelected={pathname.startsWith('/profile')}
+        onClick={() => handleSelect('/profile')}
+      />
     </Flex>
   );
 }
