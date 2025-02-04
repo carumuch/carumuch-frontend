789baa95ee1486bfacaca6329174975ee9caefaa
yeo99 <qw6375@gmail.com>
2025-02-04 21:01:15 +0900

    #2 Refactor: login, signup API 사용시 response 인터셉터에서 분기 추가, 상수 적용

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 import { ApiResponse } from '@/types/d';
+import { TOKEN_KEY } from '@/constants';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_SERVER, // 예시로 기본 URL을 설정합니다.
@@ -13,13 +14,13 @@ const axiosInstance = axios.create({
 // 요청 인터셉터
 axiosInstance.interceptors.request.use(
   (config) => {
-    const updatedConfig = { ...config }; // config 복사
-    const token = localStorage.getItem('token');
+    const updatedConfig = { ...config };
+    const token = localStorage.getItem(TOKEN_KEY);
+
     if (token) {
       updatedConfig.headers.Authorization = `Bearer ${token}`;
     }
-
-    return updatedConfig; // 수정된 config 반환
+    return updatedConfig;
   },
   (error) => Promise.reject(error),
 );
@@ -27,10 +28,15 @@ axiosInstance.interceptors.request.use(
 // 응답 인터셉터
 axiosInstance.interceptors.response.use(
   (response: AxiosResponse<ApiResponse<any>>) => {
+    // 로그인, 회원가입 API는 헤더를 살리기 위해 원본 응답 반환. 추후 분리 필요
+    if (response.config.url === '/login' || response.config.url === '/signup') {
+      return response;
+    }
     const data = response.data;
+
     if (!data.success) {
       return Promise.reject(
-        new Error(data.message) || '알 수 없는 오류가 발생하였습니다.',
+        new Error(data.message || '알 수 없는 오류가 발생하였습니다.'),
       );
     }
     // 성공 케이스라면 원하는 데이터만 반환
